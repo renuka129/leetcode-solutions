a01cc6d22d3d072c4a2fce3f52696f6392cefccc
renuka129 <118260170+renuka129@users.noreply.github.com>
2026-07-24 13:32:38 +0530

    Time: 174 ms (98.18%), Space: 47 MB (49.09%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -1,17 +1,40 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-        Set<Integer>s=new HashSet<>();
-        Set<Integer>pair=new HashSet<>();
+        // Set<Integer>s=new HashSet<>();
+        // Set<Integer>pair=new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i;j<nums.length;j++){
+        //         pair.add(nums[i]^nums[j]);
+        //     }
+        // }
+        // for(int i:pair){
+        //     for(int j=0;j<nums.length;j++){
+        //         s.add(nums[j]^i);
+        //     }
+        // }
+        // return s.size();
+        //optimized version
+        int count=0;
+        boolean ans[]=new boolean[2048];
+        boolean pair[]=new boolean[2048];
         for(int i=0;i<nums.length;i++){
             for(int j=i;j<nums.length;j++){
-                pair.add(nums[i]^nums[j]);
+            pair[nums[i]^nums[j]]=true;
+           }
+        }
+        for(int x=0;x<2048;x++){
+            if(pair[x]!=true){
+                continue;
+            }
+            for(int num:nums){
+                ans[num^x]=true;
             }
         }
-        for(int i:pair){
-            for(int j=0;j<nums.length;j++){
-                s.add(nums[j]^i);
+        for(boolean b:ans){
+            if(b){
+                count++;
             }
         }
-        return s.size();
+        return count;
     }
 }
