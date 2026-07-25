10eee7abe86a0ced0a8ea04b30ec8a6415a9f269
renuka129 <118260170+renuka129@users.noreply.github.com>
2026-07-25 22:22:49 +0530

    Time: 1 ms (100%), Space: 42.4 MB (92.77%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,13 +1,27 @@
 class Solution {
     public int maxProduct(int n) {
-        int ans=1;
-        ArrayList<Integer>l=new ArrayList<>();
+        // int ans=1;
+        // ArrayList<Integer>l=new ArrayList<>();
+        // while(n!=0){
+        //     int q=n%10;
+        //     l.add(q);
+        //     n/=10;
+        // }
+        // Collections.sort(l);
+        // return l.get(l.size()-1)*l.get(l.size()-2);
+
+        int max1=0;int max2=0;
         while(n!=0){
             int q=n%10;
-            l.add(q);
+            if(max1<=q){
+                max2=max1;
+                max1=q;
+            }
+            else if(max2<q){
+                max2=q;
+            }
             n/=10;
         }
-        Collections.sort(l);
-        return l.get(l.size()-1)*l.get(l.size()-2);
+        return max1*max2;
     }
 }
